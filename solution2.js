bb827235fc4f50b52294ed37e38f630741be04f9
Telvin <mark.telvin@student.moringaschool.com>
2024-04-01 22:36:02 +0300

    poy

---
+++ solution2.js
@@ -0,0 +1,14 @@
+function generateArray(start, end) {
+    let result = [];
+    if (start <= end) {
+      for (let i = start; i <= end; i++) {
+        result.push(i);
+      }
+    } else {
+      for (let i = start; i >= end; i--) {
+        result.push(i);
+      }
+    }
+    return result;
+  }
+  
